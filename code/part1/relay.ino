96006525aa3ac51ff139cacccbbb0a883e4fe70c
sahil <sahil11panindre@gmail.com>
2021-03-02 23:10:55 +0530

    Add files via upload

---
+++ code/part1/relay.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+}
